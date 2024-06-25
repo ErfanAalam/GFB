f87335f4cfef4d27a38f7490650c9dbccd2f35a9
ErfanAalam <erfankhansiwani@gmail.com>
2024-06-25 09:38:05 +0530

    integrated lines

--- src/screens/services/Integrated-services/index.js.js
+++ src/screens/services/Integrated-services/index.js.js
@@ -18,7 +18,7 @@ const Integrated = () => {
 
       <div class="absolute inset-0 bg-[rgb(29,30,36)] opacity-30"></div>
 
-      <img src='Assets/Services/Group 3620.png' className='absolute top-[20%] md:top-[33%] left-[45%] xsm:left-[45%] 2xsm:left-[45%] 3xsm:left-[40%] md:left-36 lg:left-52 xl:left-56 2xl:left-[4%]  z-20 md:z-20 h-[400px] xsm:h-[550px] md:h-[106%] lg:h-[112%] md:w-[70%] lg:w-[72%] xl:w-[75%] 2xl:w-[80%]' />
+      <img src='Assets/Services/Group 3620.png' className='absolute top-[20%] md:top-[33%] left-[45%] xsm:left-[45%] 2xsm:left-[45%] 3xsm:left-[40%] md:left-36 lg:left-52 xl:left-56 2xl:left-[6%]  z-20 md:z-20 h-[400px] xsm:h-[550px] md:h-[106%] lg:h-[112%] md:w-[70%] lg:w-[72%] xl:w-[75%] 2xl:w-[80%]' />
 
       <div className='flex md:flex-row flex-col relative items-center gap-10 md:gap-40 2xl:gap-40 md:p-4 lg:p-20 2xl:p-20 p-6 md:pt-0 pt-32'>
 
