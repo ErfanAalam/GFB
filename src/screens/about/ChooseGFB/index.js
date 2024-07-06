d3b2d7cc8dacd4b9bcdcea9ca58b2cb3d895bc81
ErfanAalam <erfankhansiwani@gmail.com>
2024-07-06 13:33:05 +0530

    about us image updated

--- src/screens/about/ChooseGFB/index.js
+++ src/screens/about/ChooseGFB/index.js
@@ -13,7 +13,7 @@ const ChooseGfb = () => {
       </div>
 
       <div className='w-[100%] md:w-[40%] px-8 xl:w-[30%] relative md:z-30 flex justify-center z-[100]'>
-        <img src='Assets/About us/3.png' className=' md:w-[400px] md:h-[600px] lg:h-[600px] xl:h-[600px]'></img>
+        <img src='Assets/About us/3.png' className='w-[350px] 2xsm:w-[400px] 3xsm:w-[450px] md:w-[400px] md:h-[600px] lg:h-[600px] xl:h-[600px]'></img>
       </div>
 
       <div className='text-center flex flex-col w-[100%] md:w-[60%] ml-0 md:ml-10 xl:ml-10 gap-10 xl:gap-20 items-center pr-0 md:pr-20 z-[100]'>
@@ -21,7 +21,7 @@ const ChooseGfb = () => {
           <h1 className={`text-[20px] xsm:text-[24px] md:text-3xl lg:text-4xl xl:text-[40px] tracking-normal md:tracking-[6px] text-[${colors.mainTextColor}]`}>Why Choose GFB Construction?</h1>
           <hr className='w-[45%] md:w-[85%] border-t-4' />
         </div>
-        <p className='text-white text-[12px] md:text-[24px] lg:text-[30px] xl:text-2xl tracking-[0px] px-4 '>
+        <p className='text-white text-[12px] md:text-[24px] lg:text-[30px] xl:text-2xl tracking-[0px] px-4 md:pt-0 pt-6'>
           Our focus is on delivering nothing but the best.
           <br /><br />
           We handpick our projects to guarantee each client receives our full attention and the highest standard of work.
